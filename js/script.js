a9019788882fdfbd7cd424848d86119a3617349b
ShayanMB <112359137+ShayanMB@users.noreply.github.com>
2024-04-09 22:50:41 -0500

    Implemented toggleMapType

--- js/script.js
+++ js/script.js
@@ -1,36 +1,45 @@
-function init(){
-	//alert('it works');
-	var el = document.getElementById('map');
-	var myLocation = new google.maps.LatLng(41.948463, -87.655800);
-	var mapOptions = {
-		center: myLocation,
-		zoom: 18,
-		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		mapTypeControlOptions: {
-			position: google.maps.ControlPosition.BOTTOM_CENTER
-		}
-	};
+var myMap; // Global variable to hold the map object
+var marker; // Global variable to hold the marker object
 
-	var myMap = new google.maps.Map(el, mapOptions);
+function init() {
+    var el = document.getElementById('map');
+    var myLocation = new google.maps.LatLng(41.948463, -87.655800);
+    var mapOptions = {
+        center: myLocation,
+        zoom: 18,
+        mapTypeId: google.maps.MapTypeId.SATELLITE,
+        mapTypeControlOptions: {
+            position: google.maps.ControlPosition.BOTTOM_CENTER
+        }
+    };
 
-	var marker = new google.maps.Marker({
-		position: myLocation,
-		map: myMap,
-		animation: google.maps.Animation.BOUNCE,
-		icon: 'images/cubs.png'
-	});
+    myMap = new google.maps.Map(el, mapOptions);
 
-	var contentString = '<h1>IIT Perlstein Hall</h1><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cupiditate odit optio, voluptatem placeat odio dignissimos illo magnam esse asperiores voluptas at iure vero eum, nemo aperiam? Ipsam, atque nobis rem.</p>';
+    marker = new google.maps.Marker({
+        position: myLocation,
+        map: myMap,
+        animation: google.maps.Animation.BOUNCE,
+        icon: 'images/cubs.png'
+    });
 
-	var infowindow = new google.maps.InfoWindow({
-      content: contentString
-  	});
+    var contentString = '<h1>Wrigley Field</h1>' + '<p class="info-text">Home of the Chicago Cubs!</p>';
 
-	google.maps.event.addListener(marker, 'mouseover', function() {
-    	infowindow.open(myMap, marker);
-  	});
+    var infowindow = new google.maps.InfoWindow({
+        content: contentString
+    });
 
+    // Add event listener for when the marker is hovered over
+    google.maps.event.addListener(marker, 'mouseover', function() {
+        infowindow.open(myMap, marker);
+    });
+}
 
+function toggleMapType() {
+    if (myMap.getMapTypeId() === google.maps.MapTypeId.SATELLITE) {
+        myMap.setMapTypeId(google.maps.MapTypeId.ROADMAP);
+    } else {
+        myMap.setMapTypeId(google.maps.MapTypeId.SATELLITE);
+    }
 }
 
 google.maps.event.addDomListener(window, 'load', init);
